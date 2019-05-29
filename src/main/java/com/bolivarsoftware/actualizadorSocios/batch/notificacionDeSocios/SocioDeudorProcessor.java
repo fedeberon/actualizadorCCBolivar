bc953437c529b5ba6d2b2d4ff0adeeaa98b08876
federicoberon <federico.beron@incluit.com>
2019-05-29 09:43:33 -0300

    Se agrega nuevo job para corren el primer dia del mes.

---
+++ src/main/java/com/bolivarsoftware/actualizadorSocios/batch/notificacionDeSocios/SocioDeudorProcessor.java
@@ -0,0 +1,7 @@
+package com.bolivarsoftware.actualizadorSocios.batch.notificacionDeSocios;
+
+/**
+ * Created by federicoberon on 29/05/2019.
+ */
+public class SocioDeudorProcessor {
+}
---
+++ src/main/java/com/bolivarsoftware/actualizadorSocios/batch/notificacionDeSocios/SocioDeudorReader.java
@@ -0,0 +1,7 @@
+package com.bolivarsoftware.actualizadorSocios.batch.notificacionDeSocios;
+
+/**
+ * Created by federicoberon on 28/05/2019.
+ */
+public class SocioDeudorReader {
+}
